6f6b89dcfb27c39772a1d0da8a140ce8aac558c3
KeigoAdachi <k.adachi.contact@gmail.com>
2026-05-27 11:18:37 +0900

    feat: introduce operator for passive.

--- program_files/passive/ingest/passive_operator.py
+++ program_files/passive/ingest/passive_operator.py
@@ -1,3 +1,14 @@
+from typing import List
+
+from passive.ingest.pdf.loader import PypdfLoader
+from passive.ingest.pdf._types import PDF_PATH
+from passive.ingest.pdf.chunker import TokenChunker
+from shared.schemas import Document, Chunk
 
 def passive_operator():
-    ...
+    
+    pdfloader = PypdfLoader(PDF_PATH)
+    docs: List[Document] = pdfloader.load_pdf()
+
+    pdfchunker = TokenChunker()
+    chunks: List[Chunk] = pdfchunker.make_chunk(docs)
