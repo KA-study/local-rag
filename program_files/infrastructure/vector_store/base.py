cbf218b73a683855ec60b1c0c53e861d213b87ac
KeigoAdachi <k.adachi.contact@gmail.com>
2026-05-28 00:40:00 +0900

    feat: introduce directory and files for vector_store.

---
+++ program_files/infrastructure/vector_store/base.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+from shared.schemas import EmbeddedChunk, RetrievedChunk
+
+class VectoreStore(ABC):
+
+    @abstractmethod
+    def add(self, chunks: list[EmbeddedChunk]) -> None:
+        pass
+
+    @abstractmethod
+    def search(
+        self,
+        query_embedding: list[float],
+        top_k: int,
+    ) -> list[RetrievedChunk]:
+        pass
