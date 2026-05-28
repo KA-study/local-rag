d46154869dae988b2516f35c2f23696a9b5d83b9
KeigoAdachi <k.adachi.contact@gmail.com>
2026-05-28 16:02:47 +0900

    feat: add two major methods to ChromaVectorStore class.

--- program_files/infrastructure/vector_store/chroma_store.py
+++ program_files/infrastructure/vector_store/chroma_store.py
@@ -30,6 +30,93 @@ class ChromaVectorStore(VectorStore):
         self._collection.add(**data)
 
 
+    def _to_chroma_format(self, chunks: list[EmbeddedChunk]) -> dict:
+
+        #この四つのパラメータはchromedbによって定められている
+        texts: list[str] = []
+        embeddings: list[list[float]] = []
+        metadatas: list[dict[str, Any]] = []
+        ids: list[str] = []
+
+        for chunk in chunks:
+
+            texts.append(chunk.chunk.text)
+
+            embeddings.append(chunk.embedding)
+
+            metadatas.append({
+                "page": chunk.chunk.page, 
+                "source": chunk.chunk.source,
+                "index": chunk.chunk.chunk_index
+            })
+
+            ids.append(
+                f"{chunk.chunk.source}_{chunk.chunk.page}_{chunk.chunk.chunk_index}"
+            )
+
+        return {
+            "documents": texts,
+            "embeddings": embeddings,
+            "metadatas": metadatas,
+            "ids": ids
+        }
+
+
+    def search(
+        self,
+        query_embedding: list[float],
+        top_k: int = 5
+    ) -> list[RetrievedChunk]:
+
+        result = self._collection.query(
+            query_embeddings=[query_embedding],
+            n_results=top_k
+        )
+
+        if not (
+            result["documents"] and
+            result["metadatas"] and
+            result["distances"]
+        ):
+            raise ValueError("some data in chromedb was racked.")
+
+        #batchではなく、単一のqueryであるから、[0]指定
+        texts = result["documents"][0]
+        metadatas = result["metadatas"][0]
+        distances = result["distances"][0]
+
+        retrieved_chunks: list[RetrievedChunk] = []
+
+        for text, metadata, distance in zip(
+            texts,
+            metadatas,
+            distances
+        ):
+
+            if not isinstance(metadata["page"],int):
+                raise ValueError("Invalid object type.")
+            if not isinstance(metadata["source"],str):
+                raise ValueError("Invalid object type.")
+            if not isinstance(metadata["index"],int):
+                raise ValueError("Invalid object type.")
+
+            chunk = Chunk(
+                text=text,
+                page=metadata["page"],
+                source=metadata["source"],
+                chunk_index=metadata["index"]
+            )
+
+            retrieved_chunk = RetrievedChunk(
+                chunk=chunk,
+                score=distance
+            )
+
+            retrieved_chunks.append(retrieved_chunk)
+
+        return retrieved_chunks
+
+
     def count(self) -> int:
         return self._collection.count()
 
