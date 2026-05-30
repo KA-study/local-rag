f727b774fb0d107aaf9277560ca1135856608651
KeigoAdachi <k.adachi.contact@gmail.com>
2026-05-31 00:16:04 +0900

    feat: add ClitInterface.

--- program_files/active/ui/cli.py
+++ program_files/active/ui/cli.py
@@ -1,2 +1,9 @@
+from active.ui.base import UserInterface
+
 class CliInterface:
-    ...
+
+    def get_input(self) -> str:
+        return input(">>> ")
+
+    def output(self, text: str) -> None:
+        print(text)
