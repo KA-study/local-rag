a1e37f773dabaaec95f54e2408daa9ebb4f4db40
KeigoAdachi <k.adachi.contact@gmail.com>
2026-05-31 00:15:08 +0900

    feat: add UserInterface(ClieInterface).

--- program_files/active/session/session.py
+++ program_files/active/session/session.py
@@ -1,27 +1,31 @@
 
 from active.session.history import History
 from active.query.pipeline import QueryPipeline
+from active.ui.base import UserInterface
 
 class Session:
 
+    #Sessionインスタンス化時に、CliInterfaceを引数として渡して初めて、Protocolの静的チェックが効く。
     def __init__(
         self,
         session_id: str,
         history: History,
-        pipeline: QueryPipeline
+        pipeline: QueryPipeline,
+        ui: UserInterface
     ):
 
         self._session_id = session_id
         self._history = history
         self._pipeline = pipeline
+        self._ui = ui
 
 
     def run(self):
     
         while True:
-            ...
-    
+
             #入力
+            query = self._ui.get_input()
 
             #QueryPipeline
 
--- program_files/active/ui/base.py
+++ program_files/active/ui/base.py
@@ -2,7 +2,7 @@ from typing import Protocol
 
 class UserInterface(Protocol):
 
-    def input(self) -> str:
+    def get_input(self) -> str:
         ...
 
     def output(
