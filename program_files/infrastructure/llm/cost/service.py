14a09ec14fbee1849bd838229e4d403b3903030b
KeigoAdachi <k.adachi.contact@gmail.com>
2026-06-02 18:13:51 +0900

    feat: add calc_cost function.

---
+++ program_files/infrastructure/llm/cost/service.py
@@ -0,0 +1,18 @@
+
+
+from shared.schemas import PRICE_TABLE
+
+
+def calc_cost(input_tokens: int, output_tokens: int, model_name: str) -> float:
+    
+    if model_name in PRICE_TABLE.keys():
+        cost = (
+            input_tokens*PRICE_TABLE[model_name].input_fee+
+            output_tokens*PRICE_TABLE[model_name].output_fee
+        )
+
+    else: 
+        raise ValueError(f"Unregistared model name: {model_name}.")
+
+    return cost
+ 
