8c7b7d1094a4301f2f00d1a1f0980234a312758b
KeigoAdachi <k.adachi.contact@gmail.com>
2026-06-02 17:56:56 +0900

    feat: create a outline.

---
+++ program_files/infrastructure/llm/cost/cost_manager.py
@@ -0,0 +1,20 @@
+
+
+from infrastructure.llm.cost.usage_db import SQliteUsageDB
+from shared.schemas import Usage
+
+
+class CostManager:
+
+    #ここではSqliteUsageDBなど固定だが、最終的には上から引数としてもらう形にする。
+    def __init__(self):
+        self._usage_db = SQliteUsageDB()
+
+    def check_allowance(self):
+        ...
+
+    def write_log(self, user_id: str, usage_db: Usage):
+        self._usage_db.write_log(user_id, usage_db)
+
+    def get_status(self):
+        ...
