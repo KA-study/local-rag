8b74e0f4f82a464a78fe081ff85f26776c3c0155
KeigoAdachi <k.adachi.contact@gmail.com>
2026-06-05 10:07:25 +0900

    feat: introduce SessionManager.

--- program_files/active/session/manager.py
+++ program_files/active/session/manager.py
@@ -2,20 +2,35 @@
 #ここでhistory_managerをインスタンス化する
 
 from active.session.session import Session
+from active.session.history.history_manager import HistoryManager
+from active.session._types import SessionContext
+from app.context import AppContext
 
+#過去のセッション、または新規セッションを選択できる画面（Lineでいう、フレンド一覧画面）を提供し、またそのうちのいづれかが選択されたとき、Sessionクラス内の処理に移る。
+
+"""
+セッション一覧取得
+
+セッション一覧および新規作成を表示
+
+セッション選択入力待機
+
+選択されたセッションでsession.run()
+
+コマンド確認（SessionManager終了）
+"""
 class SessionManager:
 
-    def create(self) -> Session:
-        ...
-
-    def save(self, session: Session):
-        ...
-
-    def load(
+    def __init__(
         self,
-        session_id: str
-    ) -> Session:
-        ...
+        app_context: AppContext
+    ):
+        self._app_context = app_context
 
-    def list_sessions(self):
-        ...
+    
+    def run(self):
+    
+        while True:
+
+            #セッション一覧取得
+
