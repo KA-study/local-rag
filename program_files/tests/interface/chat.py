c17958152c3b24ec93fb2cc6ad351aa1f6ff6cc5
KeigoAdachi <k.adachi.contact@gmail.com>
2026-06-08 11:58:23 +0900

    feat: add test code for ChatInterface.display_message().

--- program_files/tests/interface/chat.py
+++ program_files/tests/interface/chat.py
@@ -4,7 +4,7 @@ from interface.chat.base import ChatInterface
 from interface.chat.cli import CliChatInterface
 from shared.schemas import ExitCommandError
 
-
+#======ChatInterface.get_input()==============
 def test_get_input(monkeypatch):
 
     chat_interface: ChatInterface = CliChatInterface()
@@ -30,3 +30,18 @@ def test_get_input_exit_command(monkeypatch):
 
     with pytest.raises(ExitCommandError):
         chat_interface.get_input()
+
+
+#===========ChatInterface.display_message()=============
+def test_display_message(capsys):
+
+    interface = CliChatInterface()
+
+    interface.display_message(
+        "assistant",
+        "hello"
+    )
+
+    captured = capsys.readouterr()
+
+    assert captured.out == "assistant: hello\n\n"
