78f0329ff3370b31f297e51351e091a8b605dcb8
KeigoAdachi <k.adachi.contact@gmail.com>
2026-06-11 15:44:16 +0900

    feat: introduce Profile.

---
+++ program_files/profile/profile.py
@@ -0,0 +1,8 @@
+"""
+- Account etnry point
+- Settings entry point
+"""
+
+def profile():
+    ...
+    
---
+++ program_files/profile/settings/settings.py
@@ -0,0 +1,14 @@
+
+
+from program_files.app.context import AppContext
+
+class Settings:
+
+    def __init__(
+        self,
+        app_context: AppContext,
+    ):
+        ...
+
+    def run(self):
+        ...
