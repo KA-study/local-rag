4c6eef38389115db0861d3e71cfef929b0349e48
KeigoAdachi <k.adachi.contact@gmail.com>
2026-06-18 14:35:30 +0900

    add class: ComponentsRegistry.

---
+++ program_files/app/registry/components_registry.py
@@ -0,0 +1,55 @@
+
+from program_files.app.registry.components_info import ComponentsInfo
+
+class ComponentsRegistry:
+    _registry: dict[type, list[ComponentsInfo]] = {}
+
+    @classmethod
+    def register(
+        cls,
+        base: type,
+        components_info: ComponentsInfo,
+    ) -> None:
+
+        cls._registry.setdefault(
+            base,
+            []
+        ).append(components_info)
+
+    #decorator
+    @classmethod
+    def component(
+        cls,
+        base: type, #抽象既定クラスが入る。
+        name: str #具体クラスの名前が入る
+    ):
+
+        def decorator(
+            implementation: type
+        ) -> type:
+
+            #components_info生成
+            components_info = ComponentsInfo(
+                type = implementation,
+                name=name
+            )
+
+            cls.register(
+                base,
+                components_info
+            )
+
+            return implementation
+
+        return decorator
+
+    @classmethod
+    def get_choices(
+        cls,
+        base: type,
+    ) -> list[ComponentsInfo]:
+
+        return cls._registry.get(
+            base,
+            []
+        )
