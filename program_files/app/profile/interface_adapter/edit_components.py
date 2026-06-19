e904b0fe28733c21589d8a1a127c4fc76bdd7b67
KeigoAdachi <k.adachi.contact@gmail.com>
2026-06-19 09:06:02 +0900

    refactor: move function: build_tree from services.py to edit_components.py as method of the class.

--- program_files/app/profile/interface_adapter/edit_components.py
+++ program_files/app/profile/interface_adapter/edit_components.py
@@ -1,8 +1,10 @@
+from dataclasses import is_dataclass, fields
+
 from program_files.runtime import Runtime
 from program_files.shared.schemas import ComponentsTreeNode
 from program_files.app.context.components import Components
+from program_files.app.registry.components_registry import ComponentsRegistry
 from program_files.interface.edit_components.base import EditComponentsInterface
-from program_files.app.profile.interface_adapter.services import build_tree
 
 class EditComponentsInterfaceAdapter:
     
@@ -13,15 +15,47 @@ class EditComponentsInterfaceAdapter:
     def get_input(self):
         return self._edit_components_interface.get_input()
 
+
     def display_list(
         self,
         components: Components
     ) -> None:
 
-        components_tree: ComponentsTreeNode = build_tree(
+        components_tree: ComponentsTreeNode = self._build_tree(
             name="components",
             obj=components
         )
 
         self._edit_components_interface.display_list(components_tree)
       
+
+    #再帰処理
+    def _build_tree(
+        self,
+        name: str,
+        obj: object
+    ) -> ComponentsTreeNode:
+
+        #枝部分処理
+        if is_dataclass(obj):
+            return ComponentsTreeNode(
+                name=name,
+                current=None,
+                choices=None,
+                children=[
+                    self._build_tree(field.name, getattr(obj, field.name))
+                    for field in fields(obj)
+                ]
+            )
+
+        #葉部分処理
+        return ComponentsTreeNode(
+            name=name,
+            current=ComponentsRegistry.get_name(type[obj]),       #仮置き
+            choices=[
+                choice.name
+                for choice in ComponentsRegistry.get_choices_for_implementation(type[obj])
+            ],#仮置き
+            children=[]
+        )
+
