a0fad63759baf9b655a75b2ab58e6a585fb57a82
KeigoAdachi <k.adachi.contact@gmail.com>
2026-06-23 22:57:53 +0900

    feat: apply class: Cli and Gui to introduction of ProfileRunItnerface.

--- program_files/interface/cli.py
+++ program_files/interface/cli.py
@@ -3,10 +3,12 @@ from dataclasses import dataclass
 from program_files.interface.chat.cli import CliChatInterface
 from program_files.interface.session_manager.cli import CliSessionManagerInterface
 from program_files.interface.edit_tree.cli import CliEditTreeInterface
+from program_files.interface.profile_run.cli import CliProfileRunInterface
 
 @dataclass
 class Cli:
     chat: type[CliChatInterface] = CliChatInterface
     session_manager: type[CliSessionManagerInterface] = CliSessionManagerInterface
     edit_tree: type[CliEditTreeInterface] = CliEditTreeInterface
+    profile_run: type[CliProfileRunInterface] = CliProfileRunInterface
 
