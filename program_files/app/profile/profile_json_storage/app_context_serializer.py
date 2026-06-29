a8a4e1e34dc637c28bb193e842e261a500328ce0
KeigoAdachi <k.adachi.contact@gmail.com>
2026-06-29 17:58:30 +0900

    feat: introduce class: AppContextSerializer
    I'm going to move internal methods related to serializing and deserializing to class: AppContextSerializer.

---
+++ program_files/app/profile/profile_json_storage/app_context_serializer.py
@@ -0,0 +1,27 @@
+
+
+
+class AppContextSerializer:
+    
+    def dict_to_app_context(self):
+        ...
+
+
+    def dict_to_components(self):
+        ...
+
+
+    def dict_to_user_config(self):
+        ...
+
+
+    def app_context_to_dict(self):
+        ...
+
+
+    def components_to_dict(self):
+        ...
+
+
+    def user_config_to_dict(self):
+        ...
