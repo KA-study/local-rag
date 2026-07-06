02b56701b2e5b6e17dfac853fa2b5ceb7780a651
KeigoAdachi <k.adachi.contact@gmail.com>
2026-07-06 18:24:56 +0900

    feat: add a function at a class: AppContextGenerator allow users to specify arbitrary parameters.

--- program_files/app/profile/app_context_generator/app_context_generator.py
+++ program_files/app/profile/app_context_generator/app_context_generator.py
@@ -10,9 +10,14 @@ from program_files.app.profile.app_context_generator._types import (
 
 class AppContextGenerator:
 
-    def generate_app_context(self) -> AppContext:
+    def generate_app_context(
+        self,
+        user_id=DEFAULT_USER_ID,
+        components=DEFAULT_COMPONENTS,
+        user_config=DEFAULT_USER_CONFIG
+    ) -> AppContext:
         return AppContext(
-            user_id =  DEFAULT_USER_ID,
-            components = DEFAULT_COMPONENTS,
-            user_config = DEFAULT_USER_CONFIG
+            user_id = user_id,
+            components = components,
+            user_config = user_config
         )
