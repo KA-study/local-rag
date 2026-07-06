0d0b3c59b41883690703e127b3b44b9121e27303
KeigoAdachi <k.adachi.contact@gmail.com>
2026-07-06 18:38:28 +0900

    feat: add a function at a class: Main to create new_user_id when latest_user_id aren't found or latest_user_file aren't found.

--- program_files/main.py
+++ program_files/main.py
@@ -3,6 +3,9 @@
 from program_files.app.context.context import AppContext
 from program_files.app.profile.profile_service import ProfileService
 from program_files.main_interface_adapter import MainInterfaceAdapter
+from program_files.app.profile.interface_adapter.switch_user_interface_adapter import SwitchUserInterfaceAdapter
+from program_files.app.profile.app_context_generator.app_context_generator import AppContextGenerator
+
 
 """
 - SessionManager entry point
@@ -14,8 +17,15 @@ class Main:
 
     def run(self):
         profile_service = ProfileService()
+        
+        try:
+            app_context: AppContext = profile_service.load_latest_app_context()
+        except FileNotFoundError or KeyError:
+            user_id: str = SwitchUserInterfaceAdapter().create_user()
+            app_context: AppContext = AppContextGenerator().generate_app_context(
+                user_id=user_id
+            )
 
-        app_context: AppContext = profile_service.load_latest_app_context()
         
         interface = MainInterfaceAdapter(app_context)
 
