98825dbaae7b79cffeb4930a6d932a064b1861bf
KeigoAdachi <k.adachi.contact@gmail.com>
2026-07-07 14:25:04 +0900

    fix: add decorator of ComponentsRegistry at FakeLLM.

--- program_files/infrastructure/llm/llm_engine/fake_llm.py
+++ program_files/infrastructure/llm/llm_engine/fake_llm.py
@@ -1,8 +1,13 @@
 
-
+from program_files.app.registry.components_registry import ComponentsRegistry
 from program_files.shared.schemas import LLMResponse, Usage
 from program_files.infrastructure.llm.llm_engine.base import LLM
 
+
+@ComponentsRegistry.component(
+    base=LLM,
+    name="fake_llm"
+)
 class FakeLLM(LLM):
 
     def generate(self, prompt: str) -> LLMResponse:
